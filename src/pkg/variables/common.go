0e76d624e75754d3b3034ab1cc90b7eb86e6a799
Dave Worth <dave@defenseunicorns.com>
2026-01-12 16:08:09 -0700

    fix(lint): centralize package docs for each package
    This fixes lint errors of the form:
    
    ```
      Error: src/cmd/internal.go:4:1: package has more than one godoc ("cmd") (godoclint)
      // Package cmd contains the CLI commands for maru.
    ```
    
    (as seen in this GH Actions run: https://github.com/defenseunicorns/maru-runner/actions/runs/20803687298/job/59753460934?pr=223)
    
    making `main` red

--- src/pkg/variables/common.go
+++ src/pkg/variables/common.go
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 // SPDX-FileCopyrightText: 2023-Present the Maru Authors
 
-// Package variables contains functions for interacting with variables
 package variables
 
 import (
---
+++ src/types/doc.go
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2023-Present the Maru Authors
+
+// Package types contains all the types used by the runner.
+package types
